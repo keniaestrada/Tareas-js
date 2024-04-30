fdb8c1e44539c730ec3bd5333569fc9229b5bbb6
keniaestrada <kenialbsg@gmail.com>
2024-04-30 16:14:45 -0600

    Tarea2, segundo cambio

--- Tarea 2. Java.js
+++ Tarea 2. Java.js
@@ -109,13 +109,13 @@ const arr1 = [0, 1, 2, [3, 4]];
 console.log(arr1.flat());
 // expected output: Array [0, 1, 2, 3, 4]
 
-const arr2 = [0, 1, [2, [3, [4, 5]]]];
+let multiDimension = [1, [2, 3, [4, 5, [6]]]];
 
-console.log(arr2.flat());
+console.log(multiDimension .flat());
 // expected output: Array [0, 1, 2, Array [3, Array [4, 5]]]
 
-console.log(arr2.flat(2));
+console.log(multiDimension .flat(2));
 // expected output: Array [0, 1, 2, 3, Array [4, 5]]
 
-console.log(arr2.flat(Infinity));
+console.log(multiDimension .flat(Infinity));
 // expected output: Array [0, 1, 2, 3, 4, 5]
